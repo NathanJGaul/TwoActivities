df7580258f6e03f0a8686cbdb6ef43ac74b7787c
Nathan Gaul <nathanjgaul@gmail.com>
2019-08-20 18:22:06 -0600

    Reply intent

--- app/src/main/java/com/nathanjgaul/twoactivities/SecondActivity.java
+++ app/src/main/java/com/nathanjgaul/twoactivities/SecondActivity.java
@@ -3,9 +3,16 @@ package com.nathanjgaul.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    public static final String EXTRA_REPLY = "com.nathanjgaul.twoactivities.extra.REPLY";
+    public EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,15 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+        mReply  = findViewById(R.id.editText_second);
+    }
+
+    public void returnReply(View view) {
+        Log.d(LOG_TAG, "Button Pressed!");
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
